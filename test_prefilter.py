9fbd0dec96ae9829d9f2c55950611f688b2a1746
Anders Lehmann <anders@hih.au.dk>
2013-03-18 09:00:38 +0100

    Duh, the real test was commented out

--- test_prefilter.py
+++ test_prefilter.py
@@ -4,8 +4,10 @@ def test_format():
     a = Q('1.45 m')
     s = "{}".format(a)
     assert s == '1.45 m'
-#    s = "{:.4e}".format(a)
-#    assert s == '1.4500E+0 m'
+    s = "{:.4e}".format(a)
+    assert s == '1.4500E+0 m'
     s = "{.value}".format(a)
     assert s == '1.45'
+    s = "%s" % a
+    assert s == '1.45 m'
 
